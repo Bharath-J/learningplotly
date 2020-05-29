d77cf3faf3ca32ac6e3db0500a20c0d3c9b65dc7
bharath <bjagadis@uci.edu>
2020-05-28 23:24:26 -0700

    Nested bar

--- notebooks/1-04-BarCharts/bar1.py
+++ notebooks/1-04-BarCharts/bar1.py
@@ -7,11 +7,29 @@ df = pd.read_csv('../../notebooks/Data/2018WinterOlympics.csv')
 
 print(df.head())
 
-data = go.Bar(x=df['NOC'],
-                y=df['Total'])
+# data = go.Bar(x=df['NOC'],
+#                 y=df['Total'])
+
+trace1 = go.Bar(x=df['NOC'],
+                y=df['Gold'],
+                marker={'color':'#FFD700'},
+                name='Gold')
+
+trace2 = go.Bar(x=df['NOC'],
+                y=df['Silver'],
+                marker={'color':'#9EA0A1'},
+                name='Silver')
+
+trace3 = go.Bar(x=df['NOC'],
+                y=df['Silver'],
+                marker={'color':'#CD7F32'},
+                name='Bronze')
+
+data = [trace1,trace2,trace3]
+
 
 layout = go.Layout(title='2018 Winter Olympics Tally')
 
 fig = go.Figure(data=data, layout=layout)
 
-pyo.plot(fig, 'basic_bar.html')
+pyo.plot(fig, filename='nested_bar.html')
