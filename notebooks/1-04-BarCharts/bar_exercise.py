3199f73b733e224953c8c60f223b5a0bd5a3c68b
bharath <bjagadis@uci.edu>
2020-05-31 21:27:58 -0700

    Vertical and horizontal stacked bar charts

--- notebooks/1-04-BarCharts/bar_exercise.py
+++ notebooks/1-04-BarCharts/bar_exercise.py
@@ -4,6 +4,25 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 
 df = pd.read_csv('../../notebooks/Data/mocksurvey.csv')
-print(df.head())
+#print(df)
+#print(df.columns)
+df.rename(columns={'Unnamed: 0':"Questions"}, inplace=True)
+print(df)
 
-#trace1 = go.Bar(x=df['Unnamed:0'])
+#Horizontal Stacked Bar
+data1 = [go.Bar(y=df['Questions'],
+                 x=df[col],
+                 name=col, orientation='h') for col in df.columns if col!='Questions']
+
+data2 = [go.Bar(x=df['Questions'],
+                 y=df[col],
+                 name=col) for col in df.columns if col!='Questions']
+#print(data)
+
+layout = go.Layout(title='Mock Survey Results',barmode='stack')
+fig1 = go.Figure(data=data1, layout=layout)
+fig2 = go.Figure(data=data2, layout=layout)
+
+
+pyo.plot(fig1, filename='horizontal_bar_chart.html')
+pyo.plot(fig2, filename='vertical_bar_chart.html')
