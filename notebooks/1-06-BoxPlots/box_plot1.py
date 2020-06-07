1e41a545c96ad392f63b07c7d3cd91cd8fae7c5e
bharath <bjagadis@uci.edu>
2020-06-06 22:27:13 -0700

    Snodgrass vs Twain boxplot comparison

--- notebooks/1-06-BoxPlots/box_plot1.py
+++ notebooks/1-06-BoxPlots/box_plot1.py
@@ -2,8 +2,18 @@ import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
 
-y = [1,14,14,15,16,18,18,19,19,20,20,23,24,26,27,27,28,29,33,54]
+#y = [1,14,14,15,16,18,18,19,19,20,20,23,24,26,27,27,28,29,33,54]
 
-data = [go.Box(y=y)]
+snodgrass = [.209,.205,.196,.210,.202,.207,.224,.223,.220,.201]
+twain = [.225,.262,.217,.240,.230,.229,.235,.217]
 
-pyo.plot(data,filename='Basic-Box-Plot.html')
+#data = [go.Box(y=y)]
+#data = [go.Box(y=y,boxpoints='all',jitter=0.5, pointpos=0)]
+#data =[go.Box(y=y, boxpoints='outliers')]
+
+trace1 = go.Box(y=snodgrass, name='Snodgrass')
+trace2 = go.Box(y=twain, name='Twain')
+
+data = [trace1, trace2]
+
+pyo.plot(data,filename='Basic-Box-Plot2.html')
