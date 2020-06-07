21dcf6760833e3e529032785f7e9a90cafdf0507
bharath <bjagadis@uci.edu>
2020-06-07 12:04:28 -0700

    Male vs Female Box Plot

---
+++ notebooks/1-06-BoxPlots/box_plot2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import numpy as np
+df = pd.read_csv('../../notebooks/Data/abalone.csv')
+
+#print(df.head())
+
+df_random_male = np.random.choice(df[df['sex']=='M']['rings'],10,replace=False)
+df_random_female = np.random.choice(df[df['sex']=='F']['rings'],10,replace=False)
+
+trace1 = go.Box(y=df_random_male, name='Male')
+trace2 = go.Box(y=df_random_female, name='Female')
+
+data = [trace1, trace2]
+
+pyo.plot(data, filename='Male_vs_Female_Box_Plot.html')
