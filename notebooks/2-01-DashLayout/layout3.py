b45d1c2cd806591f8ce836f12b82412c6943dcdf
bharath <bjagadis@uci.edu>
2020-06-21 23:49:58 -0700

    Added oldfaithful file

---
+++ notebooks/2-01-DashLayout/layout3.py
@@ -0,0 +1,9 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+app = dash.Dash()
+
+df = pd.read_csv('../../notebooks/Data/OldFaithful.csv')
