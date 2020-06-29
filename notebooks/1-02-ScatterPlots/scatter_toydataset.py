488d91c69a55d67a834c9a8bf6c7da6c9f9bc845
bharath <bjagadis@uci.edu>
2020-06-28 22:57:17 -0700

    Iris sepal length vs sepal width scatter plot

--- notebooks/1-02-ScatterPlots/scatter_toydataset.py
+++ notebooks/1-02-ScatterPlots/scatter_toydataset.py
@@ -1,6 +1,31 @@
 import plotly.offline as pyo
 import plotly.graph_objs as go
 from sklearn import datasets
+import pandas as pd
 
-X = datasets.load_iris()
-print(X)
+data = datasets.load_iris()
+df = pd.DataFrame(data['data'], columns=data['feature_names'])
+df['target'] = data['target']
+print(df.head())
+
+x_data = df['sepal length (cm)']
+y_data = df['sepal width (cm)']
+
+plotly_data = [go.Scatter(x=x_data,
+                            y=y_data,
+                            mode='markers',
+                            marker=dict(
+                                size=12,
+                                color='rgb(51,204,153)',
+                                symbol='circle',
+                                line={'width':2}
+                            ))]
+
+layout = go.Layout(title='Iris Sepal Length vs Sepal Width',
+                    xaxis={'title':'Sepal Length'},
+                    yaxis={'title':'Sepal Width'},
+                    hovermode='closest')
+
+fig = go.Figure(data=plotly_data, layout=layout)
+
+pyo.plot(fig,filename='Iris_scatter.html')
