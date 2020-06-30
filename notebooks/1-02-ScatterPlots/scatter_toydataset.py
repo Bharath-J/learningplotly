afa916115f1206e636ff76c04889300bd9e89b21
bharath <bjagadis@uci.edu>
2020-06-29 23:50:36 -0700

    Adding legend

--- notebooks/1-02-ScatterPlots/scatter_toydataset.py
+++ notebooks/1-02-ScatterPlots/scatter_toydataset.py
@@ -10,22 +10,26 @@ print(df.head())
 
 x_data = df['sepal length (cm)']
 y_data = df['sepal width (cm)']
+color_data = df['target']
 
 plotly_data = [go.Scatter(x=x_data,
                             y=y_data,
                             mode='markers',
+                            name=item,
                             marker=dict(
                                 size=12,
-                                color='rgb(51,204,153)',
+                                color=color_data,
                                 symbol='circle',
                                 line={'width':2}
-                            ))]
+                            )) for item in color_data]
 
 layout = go.Layout(title='Iris Sepal Length vs Sepal Width',
                     xaxis={'title':'Sepal Length'},
                     yaxis={'title':'Sepal Width'},
-                    hovermode='closest')
+                    hovermode='closest'
+                    )
 
 fig = go.Figure(data=plotly_data, layout=layout)
+#print(help(go.Scatter))
 
 pyo.plot(fig,filename='Iris_scatter.html')
