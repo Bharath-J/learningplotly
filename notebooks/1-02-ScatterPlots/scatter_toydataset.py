2248a48a4d0869b4e4da872a87641b084e304bed
bharath <bjagadis@uci.edu>
2020-06-30 22:24:22 -0700

    Numerical legend using name property

--- notebooks/1-02-ScatterPlots/scatter_toydataset.py
+++ notebooks/1-02-ScatterPlots/scatter_toydataset.py
@@ -6,22 +6,31 @@ import pandas as pd
 data = datasets.load_iris()
 df = pd.DataFrame(data['data'], columns=data['feature_names'])
 df['target'] = data['target']
-print(df.head())
+#print(data)
 
 x_data = df['sepal length (cm)']
 y_data = df['sepal width (cm)']
-color_data = df['target']
+color_data = [0,1,2]
 
-plotly_data = [go.Scatter(x=x_data,
-                            y=y_data,
-                            mode='markers',
-                            name=item,
-                            marker=dict(
-                                size=12,
-                                color=color_data,
-                                symbol='circle',
-                                line={'width':2}
-                            )) for item in color_data]
+# plotly_data = [go.Scatter(x=x_data,
+#                             y=y_data,
+#                             mode='markers',
+#                             marker=dict(
+#                                 size=12,
+#                                 color=df['target']
+#                                 ),
+#                             name=color_data
+#                             ) ]
+plotly_data = [go.Scatter(x=df[df['target']==item]['sepal length (cm)'],
+                        y=df[df['target']==item]['sepal width (cm)'],
+                        mode='markers',
+                        name=item,
+                        marker=dict(
+                            size=12
+                        )
+                        )
+                        for item in color_data]
+
 
 layout = go.Layout(title='Iris Sepal Length vs Sepal Width',
                     xaxis={'title':'Sepal Length'},
@@ -30,6 +39,6 @@ layout = go.Layout(title='Iris Sepal Length vs Sepal Width',
                     )
 
 fig = go.Figure(data=plotly_data, layout=layout)
-#print(help(go.Scatter))
-
+# #print(help(go.Scatter))
+#
 pyo.plot(fig,filename='Iris_scatter.html')
