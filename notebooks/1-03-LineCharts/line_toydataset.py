4e9042ad1f438842bd7bcf9b421f952600847a56
bharath <bjagadis@uci.edu>
2020-07-05 23:22:30 -0700

    Messy line plot joining all the datapoints

--- notebooks/1-03-LineCharts/line_toydataset.py
+++ notebooks/1-03-LineCharts/line_toydataset.py
@@ -1,13 +1,26 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
 from sklearn import datasets
 
-app = dash.Dash()
-
 data = datasets.load_boston()
 
 df = pd.DataFrame(data['data'], columns=data['feature_names'])
+
+#print(data)
+df['TARGET'] = data['target']
 print(df.head())
+
+x_data = df['TARGET']
+y_data = df['TAX']
+
+plotly_data = [go.Scatter(x=x_data,
+                        y=y_data,
+                        mode='lines'
+                        )]
+
+layout = go.Layout(title='Boston Housing Prices by Avg Num Rooms')
+
+fig = go.Figure(data=plotly_data, layout=layout)
+
+pyo.plot(fig,filename='Boston_Housing.html')
