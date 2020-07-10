13e97cab68bc752f120c8c94d20f7282e7ef9bf8
bharath <bjagadis@uci.edu>
2020-07-09 22:29:00 -0700

    Loading wine dataset

--- notebooks/1-04-BarCharts/bar_toydataset.py
+++ notebooks/1-04-BarCharts/bar_toydataset.py
@@ -2,3 +2,6 @@ import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
 from sklearn import datasets
+
+data = datasets.load_wine()
+df = pd.DataFrame(data=['data'], columns=data['feature_names'])
