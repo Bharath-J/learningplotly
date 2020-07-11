ecc4251e82ff3e5eb99bdc8bddc62ba9eb628e45
bharath <bjagadis@uci.edu>
2020-07-10 22:41:21 -0700

    Target names column added

--- notebooks/1-04-BarCharts/bar_toydataset.py
+++ notebooks/1-04-BarCharts/bar_toydataset.py
@@ -4,4 +4,9 @@ import pandas as pd
 from sklearn import datasets
 
 data = datasets.load_wine()
-df = pd.DataFrame(data=['data'], columns=data['feature_names'])
+df = pd.DataFrame(data['data'], columns=data['feature_names'])
+df['target']=data['target']
+df['target_names']=['class_0' if item==0 else 'class_1' if item==1 else 'class_2'
+                    for item in df['target']]
+print(df.head())
+print(df.columns)
