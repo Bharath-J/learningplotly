0a6df6bc2b0245ea5e7a46f71e9c63c3ecca3cc7
bharath <bjagadis@uci.edu>
2020-07-11 22:37:20 -0700

    Mean alcohol level by alcohol class chart added

--- notebooks/1-04-BarCharts/bar_toydataset.py
+++ notebooks/1-04-BarCharts/bar_toydataset.py
@@ -10,3 +10,19 @@ df['target_names']=['class_0' if item==0 else 'class_1' if item==1 else 'class_2
                     for item in df['target']]
 print(df.head())
 print(df.columns)
+
+df_groupby = df.groupby(['target_names']).mean().reset_index()
+print(df_groupby.head())
+
+trace1 = go.Bar(x=df_groupby['target_names'],
+                y=df_groupby['alcohol'],
+                )
+
+data = [trace1]
+
+layout = go.Layout(title='Mean Alcohol Level',
+                    xaxis=dict(title='Alcohol Category'),
+                    yaxis=dict(title='Mean Alcohol Level'))
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig, filename='bar_mean_alcohol.html')
