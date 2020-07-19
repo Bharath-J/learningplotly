6aa395df6707a1962cefc888d1ca4c69c2d06c6b
bharath <bjagadis@uci.edu>
2020-07-18 22:51:39 -0700

    Input text box added

---
+++ notebooks/2-07-DashCallBacks/callback1.py
@@ -0,0 +1,14 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+
+app.layout = html.Div([
+            dcc.Input(id='my-id', value='Initial Text',type='text'),
+            html.Div(id='my-div')
+])
+
+if __name__ =='__main__':
+  app.run_server()
