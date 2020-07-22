9d503adceefcdc4c0b7ce17bc9c05883bf66c556
bharath <bjagadis@uci.edu>
2020-07-21 22:19:48 -0700

    callback output added

--- notebooks/2-07-DashCallBacks/callback1.py
+++ notebooks/2-07-DashCallBacks/callback1.py
@@ -7,8 +7,12 @@ app = dash.Dash()
 
 app.layout = html.Div([
             dcc.Input(id='my-id', value='Initial Text',type='text'),
-            html.Div(id='my-div', style={'border':'2px blue solid'})
+            html.Div(id='my-div')
 ])
+
+@app.callback(Output(component_id='my-div',component_property='children'))
+def update_output_div(input_value):
+    return "You entered: {}".format(input_value)
 
 if __name__ =='__main__':
   app.run_server()
