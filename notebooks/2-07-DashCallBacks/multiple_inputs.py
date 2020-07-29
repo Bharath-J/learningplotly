ab948922c2609e35e185d0dcd8c8e7504f917dc1
bharath <bjagadis@uci.edu>
2020-07-28 23:55:18 -0700

    Created app.layout with two dropdowns and a graph

--- notebooks/2-07-DashCallBacks/multiple_inputs.py
+++ notebooks/2-07-DashCallBacks/multiple_inputs.py
@@ -6,3 +6,21 @@ import plotly.graph_objs as go
 import pandas as pd
 
 df = pd.read_csv('../../notebooks/Data/gapminderDataFiveYear.csv')
+
+app = dash.Dash()
+# ['mpg', 'hp', 'displacement']
+features = df.columns
+
+app.layout = html.Div([
+            html.Div([
+                dcc.Dropdown(id='xaxis',
+                            options=[{'label':i, 'value':i} for i in features],
+                            value='displacement')
+            ],style={'width':'48%', 'display':'inline-block'}),
+            html.Div([
+                dcc.Dropdown(id='yaxis',
+                            options=[{'label':i, 'value':i} for i in features],
+                            value='mpg')
+            )],style={'width':'48%', 'display':'inline-block'}),
+            dcc.Graph(id='feature-graphic')
+], style={'padding':10})
