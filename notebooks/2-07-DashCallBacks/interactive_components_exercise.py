9c04c6f6917afc11efd18950c2fc4cf5778f47e4
bharath <bjagadis@uci.edu>
2020-08-05 22:08:06 -0700

    Range slider added

--- notebooks/2-07-DashCallBacks/interactive_components_exercise.py
+++ notebooks/2-07-DashCallBacks/interactive_components_exercise.py
@@ -5,3 +5,21 @@ from dash.dependencies import Input, Output
 import pandas as pd
 
 app = dash.Dash()
+
+app.layout = html.Div([
+                    dcc.RangeSlider(id='range-slider',
+                                    min=-10,
+                                    max=10,
+                                    marks={i:str(i) for i in range(-10, 11)},
+                                    value=[-1,1]),
+                    html.H1(id='product')
+
+])
+
+@app.callback(Output('product','children'),
+                    [Input('range-slider','value')])
+def update_value(value_list):
+    return value_list[0]*value_list[1]
+
+if __name__=='__main__':
+    app.run_server()
