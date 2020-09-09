7d8ef4a6096112a6d7f38048625f735e724788d6
bharath <bjagadis@uci.edu>
2020-09-08 23:52:55 -0700

    TSLA stock price stored in df

--- notebooks/2-17-CodeAlongMilestoneProject/stockpicker1.py
+++ notebooks/2-17-CodeAlongMilestoneProject/stockpicker1.py
@@ -3,12 +3,18 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas_datareader as pdata
+import pandas_datareader.data as web
+from datetime import datetime
 import os
 
 app = dash.Dash()
 
 IEX_API_KEY = os.environ.get('IEX_TOKEN')
-df = pdata.iex.daily.IEXDailyReader(symbols='AAPL',api_key=IEX_API_KEY)
+print(IEX_API_KEY)
+start = datetime(2017,1,1)
+end = datetime(2017,12,31)
+df = web.DataReader('TSLA','iex',start,end,api_key=IEX_API_KEY)
+#help(web.DataReader)
 
-#print(df)
+
+print(df)
