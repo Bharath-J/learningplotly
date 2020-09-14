0ee634a2b0ab1ab58656f13099bf254026f89527
bharath <bjagadis@uci.edu>
2020-09-13 23:47:32 -0700

    DatePickerRange added

--- notebooks/2-17-CodeAlongMilestoneProject/stockpicker1.py
+++ notebooks/2-17-CodeAlongMilestoneProject/stockpicker1.py
@@ -19,9 +19,19 @@ IEX_API_KEY = os.environ.get('IEX_TOKEN')
 
 app.layout = html.Div([
                     html.H1('Stock Ticker Dashboard'),
-                    html.H3('Enter a stock symbol'),
-                    dcc.Input(id='my_stock_picker',
-                                value='TSLA'),
+                    html.Div([html.H3('Enter a stock symbol:',style={'paddingRight':'30px'}),
+                              dcc.Input(id='my_stock_picker',
+                                        value='TSLA',
+                                        style={'fontSize':24,'width':75}
+                                        )],style={'display':'inline-block','verticalAlign':'top'}),
+                               html.Div([html.H3('Select a start and end date:'),
+                                        dcc.DatePickerRange(id='my_date_picker',
+                                                            min_date_allowed=datetime(2015,1,1),
+                                                            max_date_allowed=datetime.today(),
+                                                            start_date=datetime(2020,1,1),
+                                                            end_date=datetime.today()
+                                                            )
+                    ],style={'display':'inline-block'}),
                     dcc.Graph(id='my_graph',
                                     figure={'data':[
                                             {'x':[1,2],'y':[3,1]}
@@ -30,10 +40,13 @@ app.layout = html.Div([
 ])
 
 @app.callback(Output('my_graph','figure'),
-            [Input('my_stock_picker','value')])
-def update_graph(stock_ticker):
-    start = datetime(2020,1,1)
-    end = datetime(2020,12,31)
+            [Input('my_stock_picker','value'),
+             Input('my_date_picker','start_date'),
+             Input('my_date_picker','end_date')
+            ])
+def update_graph(stock_ticker,start_date,end_date):
+    start = datetime.strptime(start_date[:10],'%Y-%m-%d')
+    end = datetime.strptime(end_date[:10],'%Y-%m-%d')
     df = web.DataReader(stock_ticker,'iex',start,end,api_key=IEX_API_KEY)
     fig = {'data':[{'x':df.index,'y':df['close']}],
             'layout':{'title':stock_ticker}
