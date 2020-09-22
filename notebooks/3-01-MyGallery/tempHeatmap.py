7c6181597b46dd31350906b6cc6d63ea05e408f0
bharath <bjagadis@uci.edu>
2020-09-21 23:31:27 -0700

    tempHeatmap.py with Cupertino temp heatmap added

---
+++ notebooks/3-01-MyGallery/tempHeatmap.py
@@ -0,0 +1,28 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+
+app = dash.Dash()
+
+df = pd.read_csv("../../notebooks/Data/CupertinoWeather2020.csv")
+
+df['DAY'] = pd.DatetimeIndex(df['DATE']).day
+df['MONTH'] = pd.DatetimeIndex(df['DATE']).month
+
+
+data = [go.Heatmap(x=df['MONTH'],
+                    y=df['DAY'],
+                    z=df['TMAX'],
+                    colorscale='Reds')]
+
+app.layout = html.Div([
+            html.Div(dcc.Graph(id='heat-map-temp',
+                                figure={'data':data}))
+])
+
+if __name__=='__main__':
+    app.run_server()
+
+#)]
