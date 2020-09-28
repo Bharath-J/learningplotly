604e7e0d7ed82157c156d2c377d9d0f3834f4c5e
bharath <bjagadis@uci.edu>
2020-09-27 23:53:25 -0700

    Callback added

--- notebooks/3-01-MyGallery/tempHeatmap.py
+++ notebooks/3-01-MyGallery/tempHeatmap.py
@@ -27,16 +27,38 @@ options = [{'label':'Blues', 'value':'Blues'},
 
 app.layout = html.Div([
             html.Label(['Colorscale for Heatmap']),
-            dcc.Dropdown(id='color-scale-dropdown',
+            dcc.Dropdown(id='color_scale_dropdown',
                         options = options,
-                        value = ['Blues'],
-                        multi=False
+                        value = 'Blues',
+                        multi=False,
+                        style={"width":"50%"}
             ),
             html.Div(dcc.Graph(id='heat-map-temp',
                                 figure={'data':data,'layout':layout}),
                                 style={'float':'left'}
                     )
 ])
+
+
+@app.callback(Output(component_id='heat-map-temp',component_property='figure'),
+    [Input(component_id='color_scale_dropdown', component_property='value')]
+)
+
+def update_graph(color_scale_dropdown):
+    dff = df
+    data = [go.Heatmap(x=dff['MONTH'],
+                        y=dff['DAY'],
+                        z=dff['TMAX'],
+                        colorscale=color_scale_dropdown)]
+
+    layout = go.Layout(title='Max Temperature in Cupertino in 2020',
+                        xaxis=dict(title='MONTH'),
+                        yaxis=dict(title='DAY'))
+    figure = {'data':data, 'layout':layout}
+
+    return figure
+
+
 
 if __name__=='__main__':
     app.run_server()
