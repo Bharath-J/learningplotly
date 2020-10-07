cfa7d930435b40f9495e902c8c65be98988db03b
bharath <bjagadis@uci.edu>
2020-10-06 23:51:15 -0700

    make_subplots imported

--- notebooks/3-01-MyGallery/tempHeatmap.py
+++ notebooks/3-01-MyGallery/tempHeatmap.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output
 
 app = dash.Dash()
